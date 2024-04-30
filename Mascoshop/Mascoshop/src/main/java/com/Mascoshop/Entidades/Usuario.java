9bab5c2ae7ca1441e821c3c0bb4e83f952aa7094
Luisa Caro <luisacasa2018@gmail.com>
2024-04-30 09:43:17 -0500

    Se modifica conexion

--- Mascoshop/Mascoshop/src/main/java/com/Mascoshop/Entidades/Usuario.java
+++ Mascoshop/Mascoshop/src/main/java/com/Mascoshop/Entidades/Usuario.java
@@ -1,98 +1,38 @@
 package com.Mascoshop.Entidades;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-//Esta clase es una entidad JPA (Java Persistence API),
-// lo que significa que puede ser mapeada a una tabla en la base de datos.
 @Entity
 @Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
-    //Clave primaria de la tabla Id
     @Id
-    //Indica que el valor de idUsuario se generará automáticamente por la base de datos
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int idUsuario;
+    private long idUsuario;
 
     @Column(name = "nombre")
     private String nombre;
+
     @Column(name = "apellido")
     private String apellido;
+
     @Column(name = "correo")
     private String correo;
+
     @Column(name = "nombreUsuario")
     private String nombreUsuario;
+
     @Column(name = "contrasena")
     private String contrasena;
+
     @Column(name = "rol")
     private int rol;
-
-    public Usuario(int idUsuario, String nombre, String apellido, String correo, String nombreUsuario, String contrasena, int rol) {
-        this.idUsuario = idUsuario;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.correo = correo;
-        this.nombreUsuario = nombreUsuario;
-        this.contrasena = contrasena;
-        this.rol = rol;
-    }
-
-    public Usuario() {
-    }
-
-    public int getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getNombreUsuario() {
-        return nombreUsuario;
-    }
-
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
-    }
-
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
-
-    public int getRol() {
-        return rol;
-    }
-
-    public void setRol(int rol) {
-        this.rol = rol;
-    }
 }
+
+
