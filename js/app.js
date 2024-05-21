93044a9ee03d7cc98142426981403794c99329a6
JossBetancur <jbetancurcanate@gmail.com>
2024-05-21 11:29:26 -0500

    Navbar responsive

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu-btn");
+const closeMenuBtn = document.querySelector(".close-menu-btn"); 
+
+[openMenuBtn, closeMenuBtn].forEach((btn) =>{
+   btn.addEventListener("click", () => {
+       menu.classList.toggle("open");
+       menu.style.transition = "transform 0.5s ease";
+   });
+});
+
+
+menu.addEventListener("transitionend", function(){
+   this.removeAttribute("style");
+});
+
+menu.querySelectorAll(".dropdown > .productos").forEach((arrow) => {
+   arrow.addEventListener("click", function() {
+       this.closest(".dropdown").classList.toggle("active");
+   });
+});
+
+
+// FinMenu
+
 gsap.registerPlugin(ScrollTrigger);
 
 const lenis = new Lenis();
@@ -11,20 +36,32 @@ requestAnimationFrame(raf);
 
 document.addEventListener('DOMContentLoaded', function(){
     var typed = new Typed('.cambio', {
-        strings: ['Mascotas', 'Gatos', 'Perros'],
+        strings: ['Gatos', 'Perros'],
         typeSpeed: 50,
         backSpeed: 30, 
         loop: true
     });
 });
 
-document.addEventListener("DOMContentLoaded", function(){
-    const navbar = document.getElementById("navbar");
-    window.addEventListener("scroll", function(){
-        if(window.scrollY > 50){
-            navbar.classList.add("scrolled");
-        }else{
-            navbar.classList.remove("scrolled");
-        }
-    });
-});
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("nav");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
+
+ /*------------------ Menu --------------*/
