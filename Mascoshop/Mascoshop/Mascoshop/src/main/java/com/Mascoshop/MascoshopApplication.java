8bd05cb29fdd0923e020fb56180105b01db095ef
Jostin <jbetancurcanate@gmail.com>
2024-05-27 16:43:23 -0500

    Faltante

--- Mascoshop/Mascoshop/Mascoshop/src/main/java/com/Mascoshop/MascoshopApplication.java
+++ Mascoshop/Mascoshop/Mascoshop/src/main/java/com/Mascoshop/MascoshopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MascoshopApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MascoshopApplication.class, args);
 	}
