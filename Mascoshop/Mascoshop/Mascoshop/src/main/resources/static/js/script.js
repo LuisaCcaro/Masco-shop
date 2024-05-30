9d0adf67ce5c0496960c0ee3e4112f1465ee0e7b
JossBetancur <jbetancurcanate@gmail.com>
2024-05-30 10:59:12 -0500

    cambio etiqueta Log In - Log Out

---
+++ Mascoshop/Mascoshop/Mascoshop/src/main/resources/static/js/script.js
@@ -0,0 +1,69 @@
+function signIn(){
+    let oauthEndpoint = "https://accounts.google.com/o/oauth2/v2/auth"
+
+    let form = document.createElement('form')
+    form.setAttribute('method', ' GET')
+    form.setAttribute('action', oauthEndpoint)
+
+
+    let params = {
+        "client_id":"1032767023549-eceqr0m60drtu1r9m3smnhkht2encr35.apps.googleusercontent.com",
+        "redirect_uri":"http://localhost:9090/index.html",
+        "response_type":"token",
+        "scope":"https://www.googleapis.com/auth/userinfo.profile",
+        "include_granted_scopes":"true",
+        'state':'pass-through-value'
+    }
+
+    for(var p in params){
+        let input = document.createElement('input')
+        input.setAttribute('type', 'hidden')
+        input.setAttribute('name', p)
+        input.setAttribute('value', params[p])
+        form.appendChild(input)
+    }
+
+    document.body.appendChild(form)
+
+    form.submit()
+}
+
+function handleAuthResponse() {
+    let hash = window.location.hash.substr(1);
+    let result = hash.split('&').reduce((res, item) => {
+        let parts = item.split('=');
+        res[parts[0]] = parts[1];
+        return res;
+    }, {});
+
+    if (result.access_token) {
+        localStorage.setItem('googleToken', result.access_token);
+        updateUIForLoggedInUser();
+        showMessage('Inicio de sesión exitoso');
+    }
+}
+
+function updateUIForLoggedInUser() {
+    let loginButton = document.getElementById('loginButton');
+    let logoutButton = document.getElementById('logoutButton');
+
+    if (localStorage.getItem('googleToken')) {
+        loginButton.style.display = 'none';
+        logoutButton.style.display = 'block';
+    } else {
+        loginButton.style.visibility = 'block';
+        logoutButton.style.display = 'none';
+    }
+}
+
+function signOut() {
+    localStorage.removeItem('googleToken');
+    updateUIForLoggedOutUser();
+    showMessage('Sesión cerrada');
+    cancelSignOut();
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    handleAuthResponse();
+    updateUIForLoggedInUser();
+});
