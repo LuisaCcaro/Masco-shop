b6adf4df7419e5eeb1d84f53826d9fbc22e79ac4
JossBetancur <jbetancurcanate@gmail.com>
2024-05-31 09:23:46 -0500

    animacion carga

---
+++ Mascoshop/Mascoshop/Mascoshop/src/main/resources/static/js/inicio.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const lenis = new Lenis();
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    var typed = new Typed('.cambio', {
+        strings: ['Gatos', 'Perros'],
+        typeSpeed: 50,
+        backSpeed: 30, 
+        loop: true
+    });
+
+    // window.addEventListener("scroll", function(){
+    //     var nav = document.querySelector("nav");
+    //     nav.classList.toggle("sticky", window.scrollY > 5);
+    // })
+
+
+
+    let animado = document.querySelectorAll(".animado");
+
+    function mostrarScroll(){
+        let scrollTop = document.documentElement.scrollTop;
+        for(var i = 0; i < animado.length; i++){
+            let alturaAnimado = animado[i].offsetTop;
+            if(alturaAnimado - 600 < scrollTop){
+                animado[i].style.opacity = 1;
+            }
+        }
+    }
+    window.addEventListener('scroll', mostrarScroll);
+
+    ScrollReveal({
+        reset: false,
+        distance: '20px',
+        duration: 1800,
+        delay: 200
+    });
+    ScrollReveal().reveal('.atencionCliente', { delay: 400 });
+    ScrollReveal().reveal('.logo', { delay: 400 });
+    ScrollReveal().reveal('.login', { delay: 400 });
+    ScrollReveal().reveal('.wrapper', { delay: 500 });
+    ScrollReveal().reveal('.info', { delay: 600 });
+    ScrollReveal().reveal('.portada', { delay: 600 });
+});
+
