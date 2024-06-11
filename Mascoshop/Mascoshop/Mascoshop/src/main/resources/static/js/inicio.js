d7857df9f5cfc69ed4d4642142277b29c141c377
JossBetancur <jbetancurcanate@gmail.com>
2024-06-11 18:10:27 -0500

    Pagina inicio lista

--- Mascoshop/Mascoshop/Mascoshop/src/main/resources/static/js/inicio.js
+++ Mascoshop/Mascoshop/Mascoshop/src/main/resources/static/js/inicio.js
@@ -156,3 +156,18 @@ searchInput.addEventListener('input', () => {
         });
     }
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // loop: true,
+    // loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
